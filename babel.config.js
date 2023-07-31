1457f49f644282e3a4264d0e70a10362215de20c
A. Alani <abdulrahmanalani95@gmail.com>
2023-07-31 15:48:23 -0700

    Remove yoast babel config dep
    move the package locally.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-	presets: [ "@yoast/babel-preset" ],
+module.exports = ( api ) => {
+	api.cache( true );
+
+	return {
+		presets: [ "@wordpress/babel-preset-default" ],
+		plugins: [ "@babel/plugin-proposal-optional-chaining", "@babel/plugin-transform-runtime" ],
+		sourceType: "unambiguous",
+	};
 };
+
