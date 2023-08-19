f4470ea3b55f933bdbd0c9221c31e4753e29269e
wpalani <abdulrahmanalani95@gmail.com>
2023-08-18 17:22:18 -0700

    Autocomplete element props doc

--- docs/partials/autocomplete/index.js
+++ docs/partials/autocomplete/index.js
@@ -1,2 +1,3 @@
 export { default as AutocompleteDemo } from './AutocompleteDemo'; 
 export { default as AutocompleteValidationVariants } from './_autocomplete-validation.mdx';
+export { default as autocompleteProps } from './props';
---
+++ docs/partials/autocomplete/props.js
@@ -0,0 +1,63 @@
+const autocompleteProps = {
+    id: {
+        type: 'string',
+        description: 'HTML id attribute.',
+        isRequired: true,
+    },
+    value: {
+        type: ['string', 'number', 'bool'],
+    },
+    placeholder: {
+        type: 'string',
+    },
+    children: {
+        type: 'node',
+        description: 'Pass `Autocomplete.Option` components as children',
+    },
+    selectedLabel: {
+        type: 'string',
+        description: 'The label of the selected option.',
+    },
+    label: {
+        type: 'string',
+        description: 'The label of the autocomplete input.',
+    },
+    labelProps: {
+        type: 'object',
+    },
+    labelSuffix: {
+        type: 'node',
+    },
+    onChange: {
+        type: 'function',
+        description: 'Callback function that is fired when an item has been selected.',
+        isRequired: true,
+    },
+    onQueryChange: {
+        type: 'function',
+        description: 'Callback function that is fired when the query changes.',
+        isRequired: true,
+    },
+    validation: {
+        type: '{ variant: string, message: node }',
+        description: 'Validation variant and message.',
+    },
+    className: {
+        type: 'string',
+    },
+    nullable: {
+        type: 'bool',
+        description: 'If true, the user can clear the selected value with `X` button.',
+    },
+    buttonProps: {
+        type: 'object',
+        description: 'Props to be passed to the `X` button element.',
+    },
+    clearButtonScreenReaderText: {
+        type: 'string',
+        description: 'Screen reader text for the clear button.',
+        defaultValue: 'Clear',
+    }
+}
+
+export default autocompleteProps;
