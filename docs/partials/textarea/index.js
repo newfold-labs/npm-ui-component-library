1659f711df210af9da37fb0cac715fa17f84dbeb
wpalani <abdulrahmanalani95@gmail.com>
2023-08-22 21:52:27 -0700

    Textarea element doc

---
+++ docs/partials/textarea/index.js
@@ -0,0 +1,1 @@
+export { default as textareaProps } from './textarea-props';
---
+++ docs/partials/textarea/textarea-props.js
@@ -0,0 +1,15 @@
+const textareaProps = {
+    rows: {
+        type: 'number',
+        defaultValue: '2',
+    },
+    disabled: {
+        type: 'bool',
+        defaultValue: 'false',
+    },
+    className: {
+        type: 'string',
+    }
+};
+
+export default textareaProps;
