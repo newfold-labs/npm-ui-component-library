3c0e009f8ab13b50fb34ca847f08d758c065bc88
wpalani <abdulrahmanalani95@gmail.com>
2023-08-24 23:05:20 -0700

    Card component docs

---
+++ docs/partials/card/card-props.js
@@ -0,0 +1,12 @@
+const cardProps = {
+    children: {
+        type: 'node',
+        description: 'The content of the card.',
+        isRequired: true,
+    },
+    className: {
+        type: 'string',
+    }
+};
+
+export default cardProps;
---
+++ docs/partials/card/index.js
@@ -0,0 +1,1 @@
+export { default as cardProps } from './card-props';
